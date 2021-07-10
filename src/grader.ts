88117828f7d16bd9d5846f639fcde91c221cf48d
Jeffrey Meng <jeffkmeng@gmail.com>
2021-07-10 00:39:40 -0700

    Update grader.ts

--- src/grader.ts
+++ src/grader.ts
@@ -58,14 +58,14 @@ export async function grade(
         if (language === Language.CPP) {
             compileResult = await getIsolateOutput(
                 box,
-                `sudo isolate -b 0 -p -E PATH --meta=tmp/${fileName}.meta --run /usr/bin/g++ -- -std=c++17 -o ${fileName} -O2 ` +
+                `sudo isolate -b 0 -p -E PATH --meta=./tmp/${fileName}.meta --run /usr/bin/g++ -- -std=c++17 -o ${fileName} -O2 ` +
                     `-Im ${fileName}.cpp`,
                 fileName
             );
         } else if (language === Language.JAVA) {
             compileResult = await getIsolateOutput(
                 box,
-                `sudo isolate -b 0 -p -E PATH --meta=tmp/${fileName}.meta --run /usr/lib/jvm/java-11-openjdk-amd64/bin/javac ${fileName}.java`,
+                `sudo isolate -b 0 -p -E PATH --meta=./tmp/${fileName}.meta --run /usr/lib/jvm/java-11-openjdk-amd64/bin/javac ${fileName}.java`,
                 fileName
             );
         }
@@ -90,20 +90,20 @@ export async function grade(
             if (language === Language.CPP) {
                 runResult = await getIsolateOutput(
                     box,
-                    `sudo isolate --cg --stderr=${fileName}.err --stdin=${fileName}.in --stdout=${fileName}.out --meta=tmp/${fileName}.meta --mem=256000 ` +
+                    `sudo isolate --cg --stderr=${fileName}.err --stdin=${fileName}.in --stdout=${fileName}.out --meta=./tmp/${fileName}.meta --mem=256000 ` +
                         `--time=2 --extra-time=1 --wall-time=10 --run ./${fileName}`,
                     fileName
                 );
             } else if (language === Language.JAVA) {
                 runResult = await getIsolateOutput(
                     box,
-                    `sudo isolate --cg --stderr=${fileName}.err --stdin=${fileName}.in --stdout=${fileName}.out --meta=tmp/${fileName}.meta --time=5 --extra-time=2 --wall-time=10 -p -d /etc --run /usr/bin/java -- -Xss256m ${fileName}`,
+                    `sudo isolate --cg --stderr=${fileName}.err --stdin=${fileName}.in --stdout=${fileName}.out --meta=./tmp/${fileName}.meta --time=5 --extra-time=2 --wall-time=10 -p -d /etc --run /usr/bin/java -- -Xss256m ${fileName}`,
                     fileName
                 );
             } else if (language === Language.PYTHON) {
                 runResult = await getIsolateOutput(
                     box,
-                    `sudo isolate --cg --stderr=${fileName}.err --stdin=${fileName}.in --stdout=${fileName}.out --meta=tmp/${fileName}.meta --mem=256000 --time=5 --extra-time=2 --wall-time=10 --env=HOME=/home/user --run /usr/bin/python3 ${fileName}.py`,
+                    `sudo isolate --cg --stderr=${fileName}.err --stdin=${fileName}.in --stdout=${fileName}.out --meta=./tmp/${fileName}.meta --mem=256000 --time=5 --extra-time=2 --wall-time=10 --env=HOME=/home/user --run /usr/bin/python3 ${fileName}.py`,
                     fileName
                 );
             }
