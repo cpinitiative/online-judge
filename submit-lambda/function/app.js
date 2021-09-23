9c460614deca24afed6dae7a8046f13f7bd14b57
Nathan Wang <nathan.r.wang@gmail.com>
2021-09-22 22:26:54 -0700

    improve submit lambda dockerfile

--- submit-lambda/function/app.js
+++ submit-lambda/function/app.js
@@ -1,10 +1,13 @@
 const aws = require("aws-sdk");
+const { v4: uuidv4 } = require('uuid');
 
 const lambda = new aws.Lambda({
   region: "us-west-1"
 });
+const docClient = new aws.DynamoDB.DocumentClient();
 
 exports.handler = function (event, context, callback) {
+  const submissionId = uuidv4();
   lambda.invoke({
     FunctionName: 'execute',
     Payload: JSON.stringify({
@@ -19,7 +22,7 @@ exports.handler = function (event, context, callback) {
       context.done('error', error);
     }
     if(data.Payload){
-     context.succeed(data.Payload)
+      context.succeed(data.Payload)
     }
   });
 }
