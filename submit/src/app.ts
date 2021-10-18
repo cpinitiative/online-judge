8966598ee768e75f20b41cf3ecc471e246568adf
Mrinall <bossguy124567@gmail.com>
2021-10-18 11:05:12 -0400

    Finish structure and integrate dynamo

--- submit/src/app.ts
+++ submit/src/app.ts
@@ -2,7 +2,7 @@ import { APIGatewayProxyEvent, APIGatewayProxyResult, AWS } from "aws-lambda";
 import { buildResponse, extractTimingInfo } from "./utils";
 import { LambdaClient, InvokeCommand } from "@aws-sdk/client-lambda";
 import { DynamoDBClient, CreateTableCommand, CreateTableCommandInput } from "@aws-sdk/client-dynamodb";
-
+import { uuid } from 'uuidv4';
 
 
 const client = new LambdaClient({
@@ -26,178 +26,39 @@ export const lambdaHandler = async (
   event: APIGatewayProxyEvent
 ): Promise<APIGatewayProxyResult> => {
   const requestData = JSON.parse(event.body || "{}");
-
+  let body;
 
   switch (event.routeKey) {
     case 'POST /submissions': // create a new problem submission (POST)
+      const submissionID = uuid();
+      // todo validate structure of body?
 
       break;
     case 'POST /execute':
 
-      // todo validate structure of body?
-      if (!requestData.language) {
-        return {
-          statusCode: 400,
-          headers: {
-            "Content-Type": "application/json",
-            "Access-Control-Allow-Origin": "*",
-          },
-          body: JSON.stringify({
-            message: "Unknown language.",
-          }),
-        };
-      }
+      return execute(requestData, event);
 
-      const compileCommand = new InvokeCommand({
-        FunctionName: "online-judge-ExecuteFunction",
-        Payload: Buffer.from(
-            JSON.stringify({
-              type: "compile",
-              language: requestData.language,
-              compilerOptions: requestData.compilerOptions,
-              filename: requestData.filename,
-              sourceCode: requestData.sourceCode,
-            }),
-            "utf-8"
-        ),
-      });
-      const compileResponse = await client.send(compileCommand);
-      const compileData = JSON.parse(
-          Buffer.from(compileResponse.Payload!).toString()
-      );
-
-      if (compileData.status === "compile_error") {
-        return {
-          statusCode: 200,
-          headers: {
-            "Content-Type": "application/json",
-            "Access-Control-Allow-Origin": "*",
-          },
-          body: Buffer.from(compileResponse.Payload!).toString(),
-        };
-      } else if (compileData.status === "internal_error") {
-        return {
-          statusCode: 500,
-          headers: {
-            "Content-Type": "application/json",
-            "Access-Control-Allow-Origin": "*",
-          },
-          body: JSON.stringify({
-            status: "internal_error",
-            message: "Compilation failed with an internal error",
-            debugData: compileData,
-          }),
-        };
-      } else if (compileData.status !== "success") {
-        return {
-          statusCode: 500,
-          headers: {
-            "Content-Type": "application/json",
-            "Access-Control-Allow-Origin": "*",
-          },
-          body: JSON.stringify({
-            status: "internal_error",
-            message: "Compilation failed with an unknown error",
-            debugData: compileData,
-          }),
-        };
-      }
-
-      const executeCommand = new InvokeCommand({
-        FunctionName: "online-judge-ExecuteFunction",
-        Payload: Buffer.from(
-            JSON.stringify({
-              type: "execute",
-              payload: compileData.output,
-              input: requestData.input,
-              timeout: 5000,
-            })
-        ),
-      });
-      const executeResponse = await client.send(executeCommand);
-      const executeData: ExecuteResult = JSON.parse(
-          Buffer.from(executeResponse.Payload!).toString()
-      );
-
-      if (executeData.status !== "success") {
-        return {
-          statusCode: 500,
-          headers: {
-            "Content-Type": "application/json",
-            "Access-Control-Allow-Origin": "*",
-          },
-          body: JSON.stringify({
-            status: "internal_error",
-            message: "Execution failed for an unknown reason",
-            debugData: executeData,
-          }),
-        };
-      }
-
-      const { stdout, exitCode, exitSignal, processError } = executeData;
-
-      const {
-        restOfString: stderr,
-        time,
-        memory,
-      } = extractTimingInfo(executeData.stderr);
-
-      let requestJSON = JSON.parse(event.body);
-
-
-      // 124 is the exit code returned by linux `timeout` command
-      if (exitCode === 124) {
-        let statusList = [{ status: "time_limit_exceeded", // get the test cases from S3 bucket;
-          // TODO: Edit execute function such that it returns an array of test cases
-          stdout,
-          stderr,
-          time,
-          memory,
-          exitCode,}]
-
-        await dynamo
-            .put({
-              TableName: "statusTable",
-              Item: {
-                submissionId: "ewksdn",
-                problemId: "fkwf",
-                timestamp: 1,
-                compileOutput: "wfewef",
-                status: statusList,
-              }
-            })
-            .promise();
-
-        return buildResponse({
-          status: "time_limit_exceeded",
-          stdout,
-          stderr,
-          time,
-          memory,
-          exitCode,
-        });
-      }
-
-      if (exitCode !== 0) {
-        return buildResponse({
-          status: "runtime_error",
-          stdout,
-          stderr,
-          time,
-          memory,
-          exitCode,
-        });
-      }
-
-      return buildResponse({
-        status: "success",
-        stdout,
-        stderr,
-        time,
-        memory,
-      });
       break;
     case 'GET /submissions/{submissionId}':
+      body = await dynamo
+          .get({
+            TableName: "statusTable",
+            Key: {
+              id: event.pathParameters.id
+            }
+          })
+          .promise();
+
+      return {
+        statusCode: 500,
+        headers: {
+          'Content-Type': 'application/json',
+          "Access-Control-Allow-Origin": "*",
+        },
+        body: JSON.stringify({
+          body
+        })
+      };
 
       break;
     default:
@@ -214,3 +75,169 @@ export const lambdaHandler = async (
   }
 
 };
+
+const execute = async (requestData, event) => {
+  if (!requestData.language) {
+    return {
+      statusCode: 400,
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+      },
+      body: JSON.stringify({
+        message: "Unknown language.",
+      }),
+    };
+  }
+
+  const compileCommand = new InvokeCommand({
+    FunctionName: "online-judge-ExecuteFunction",
+    Payload: Buffer.from(
+        JSON.stringify({
+          type: "compile",
+          language: requestData.language,
+          compilerOptions: requestData.compilerOptions,
+          filename: requestData.filename,
+          sourceCode: requestData.sourceCode,
+        }),
+        "utf-8"
+    ),
+  });
+  const compileResponse = await client.send(compileCommand);
+  const compileData = JSON.parse(
+      Buffer.from(compileResponse.Payload!).toString()
+  );
+
+  if (compileData.status === "compile_error") {
+    return {
+      statusCode: 200,
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+      },
+      body: Buffer.from(compileResponse.Payload!).toString(),
+    };
+  } else if (compileData.status === "internal_error") {
+    return {
+      statusCode: 500,
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+      },
+      body: JSON.stringify({
+        status: "internal_error",
+        message: "Compilation failed with an internal error",
+        debugData: compileData,
+      }),
+    };
+  } else if (compileData.status !== "success") {
+    return {
+      statusCode: 500,
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+      },
+      body: JSON.stringify({
+        status: "internal_error",
+        message: "Compilation failed with an unknown error",
+        debugData: compileData,
+      }),
+    };
+  }
+
+  const executeCommand = new InvokeCommand({
+    FunctionName: "online-judge-ExecuteFunction",
+    Payload: Buffer.from(
+        JSON.stringify({
+          type: "execute",
+          payload: compileData.output,
+          input: requestData.input,
+          timeout: 5000,
+        })
+    ),
+  });
+  const executeResponse = await client.send(executeCommand);
+  const executeData: ExecuteResult = JSON.parse(
+      Buffer.from(executeResponse.Payload!).toString()
+  );
+
+  if (executeData.status !== "success") {
+    return {
+      statusCode: 500,
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+      },
+      body: JSON.stringify({
+        status: "internal_error",
+        message: "Execution failed for an unknown reason",
+        debugData: executeData,
+      }),
+    };
+  }
+
+  const {stdout, exitCode, exitSignal, processError} = executeData;
+
+  const {
+    restOfString: stderr,
+    time,
+    memory,
+  } = extractTimingInfo(executeData.stderr);
+
+  let requestJSON = JSON.parse(event.body);
+
+
+  // 124 is the exit code returned by linux `timeout` command
+  if (exitCode === 124) {
+    let statusList = [{
+      status: "time_limit_exceeded", // get the test cases from S3 bucket;
+      // TODO: Edit execute function such that it returns an array of test cases
+      stdout: stdout,
+      stderr: stderr,
+      time: time,
+      memory: memory,
+      exitCode: exitCode,
+    }]
+
+    // await dynamo
+    //     .put({
+    //       TableName: "statusTable",
+    //       Item: {
+    //         submissionId: submissionID,
+    //         problemId: "fkwf",
+    //         timestamp: 1,
+    //         compileOutput: "wfewef",
+    //         status: statusList,
+    //       }
+    //     })
+    //     .promise();
+
+    return buildResponse({
+      status: "time_limit_exceeded",
+      stdout,
+      stderr,
+      time,
+      memory,
+      exitCode,
+    });
+  }
+
+  if (exitCode !== 0) {
+    return buildResponse({
+      status: "runtime_error",
+      stdout,
+      stderr,
+      time,
+      memory,
+      exitCode,
+    });
+  }
+
+  return buildResponse({
+    status: "success",
+    stdout,
+    stderr,
+    time,
+    memory,
+  });
+}
