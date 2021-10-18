7907e04395d575ee9c404f93ee819f44f7902150
Mrinall <bossguy124567@gmail.com>
2021-10-17 21:43:24 -0400

    Differenciate between requests

--- submit/src/app.ts
+++ submit/src/app.ts
@@ -8,6 +8,103 @@ const client = new LambdaClient({
 export const lambdaHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   const requestData = JSON.parse(event.body || "{}");
 
+   const operation = event.operation;
+
+  switch (operation) {
+    case 'create': // create a new problem submission (POST)
+
+      const compileCommand = new InvokeCommand({
+        FunctionName: "online-judge-ExecuteFunction",
+        Payload: Buffer.from(
+            JSON.stringify({
+              type: "compile",
+              language: requestData.language,
+              compilerOptions: requestData.compilerOptions,
+              filename: requestData.filename,
+              sourceCode: requestData.sourceCode,
+            }),
+            "utf-8"
+        ),
+      });
+      const compileResponse = await client.send(compileCommand);
+      const compileData = JSON.parse(
+          Buffer.from(compileResponse.Payload!).toString()
+      );
+
+      if(compileData.status === "compile error"){
+        return {
+          statusCode: 200,
+          headers: {
+            'Content-Type': 'application/json',
+            "Access-Control-Allow-Origin": "*",
+          },
+          body: Buffer.from(compileResponse.Payload!).toString()
+        }
+      }
+      else if (compileData.status === "runtime error"){
+        return {
+          statusCode: 100,
+          headers: {
+            'Content-Type': 'application/json',
+            "Access-Control-Allow-Origin": "*",
+          },
+          body: Buffer.from(compileResponse.Payload!).toString()
+        }
+      }
+      else if (compileData.status !== "success") {
+        // todo better error handling?
+        return {
+          statusCode: 500,
+          headers: {
+            'Content-Type': 'application/json',
+            "Access-Control-Allow-Origin": "*",
+          },
+          body: Buffer.from(compileResponse.Payload!).toString()
+        };
+      }
+
+      const executeCommand = new InvokeCommand({
+        FunctionName: "online-judge-ExecuteFunction",
+        Payload: Buffer.from(
+            JSON.stringify({
+              type: "execute",
+              payload: compileData.output,
+              input: requestData.input,
+            })
+        ),
+      });
+      const executeResponse = await client.send(executeCommand);
+      // const executeData = JSON.parse(
+      //   Buffer.from(executeResponse.Payload!).toString()
+      // );
+
+      return {
+        statusCode: 200,
+        headers: {
+          'Content-Type': 'application/json',
+          "Access-Control-Allow-Origin": "*",
+        },
+        body: Buffer.from(executeResponse.Payload!).toString()
+      };
+      break;
+    case 'read': // get a problem submission (GET)
+
+      break;
+    case 'put': //  execute a single test-case (PUT)
+
+      break;
+    default:
+      return {
+        statusCode: 100,
+        headers: {
+          'Content-Type': 'application/json',
+          "Access-Control-Allow-Origin": "*",
+        },
+        body: JSON.stringify({
+            result: "Inavalid event"
+        })
+      };
+  }
   // todo validate structure of body?
   if (!requestData.language) {
     return {
@@ -21,78 +118,5 @@ export const lambdaHandler = async (event: APIGatewayProxyEvent): Promise<APIGat
       })
     };
   }
-  
-  const compileCommand = new InvokeCommand({
-    FunctionName: "online-judge-ExecuteFunction",
-    Payload: Buffer.from(
-      JSON.stringify({
-        type: "compile",
-        language: requestData.language,
-        compilerOptions: requestData.compilerOptions,
-        filename: requestData.filename,
-        sourceCode: requestData.sourceCode,
-      }),
-      "utf-8"
-    ),
-  });
-  const compileResponse = await client.send(compileCommand);
-  const compileData = JSON.parse(
-    Buffer.from(compileResponse.Payload!).toString()
-  );
 
-  if(compileData.status === "compile error"){
-    return {
-      statusCode: 200,
-      headers: {
-        'Content-Type': 'application/json',
-        "Access-Control-Allow-Origin": "*",
-      },
-      body: Buffer.from(compileResponse.Payload!).toString()
-    }
-  }
-  else if (compileData.status === "runtime error"){
-    return {
-      statusCode: 100,
-      headers: {
-        'Content-Type': 'application/json',
-        "Access-Control-Allow-Origin": "*",
-      },
-      body: Buffer.from(compileResponse.Payload!).toString()
-    }
-  }
-  else if (compileData.status !== "success") {
-    // todo better error handling?
-    return {
-      statusCode: 500,
-      headers: {
-        'Content-Type': 'application/json',
-        "Access-Control-Allow-Origin": "*",
-      },
-      body: Buffer.from(compileResponse.Payload!).toString()
-    };
-  }
-
-  const executeCommand = new InvokeCommand({
-    FunctionName: "online-judge-ExecuteFunction",
-    Payload: Buffer.from(
-      JSON.stringify({
-        type: "execute",
-        payload: compileData.output,
-        input: requestData.input,
-      })
-    ),
-  });
-  const executeResponse = await client.send(executeCommand);
-  // const executeData = JSON.parse(
-  //   Buffer.from(executeResponse.Payload!).toString()
-  // );
-
-  return {
-    statusCode: 200,
-    headers: {
-      'Content-Type': 'application/json',
-      "Access-Control-Allow-Origin": "*",
-    },
-    body: Buffer.from(executeResponse.Payload!).toString()
-  };
 };
