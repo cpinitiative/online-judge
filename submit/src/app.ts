5f1ae76ee5cc28ae67b2d37a0fd99c9a032c9ecb
Nathan Wang <nathan.r.wang@gmail.com>
2021-10-30 18:51:57 -0700

    feat: count number of submissions, closes #5

--- submit/src/app.ts
+++ submit/src/app.ts
@@ -1,8 +1,13 @@
+import { DynamoDBClient, UpdateItemCommand } from "@aws-sdk/client-dynamodb";
 import { LambdaClient, InvokeCommand } from "@aws-sdk/client-lambda";
-import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
+import type { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
 import { buildResponse, extractTimingInfo } from "./utils";
 
 const client = new LambdaClient({
+  region: "us-west-1",
+});
+
+const dynamoDBClient = new DynamoDBClient({
   region: "us-west-1",
 });
 
@@ -17,6 +22,81 @@ interface ExecuteResult {
 }
 
 // Note: for problem submission, some flags to keep in mind: http://usaco.org/index.php?page=instructions
+
+// WIP
+interface CodeExecutionRequestData {
+  language: "cpp" | "java" | "py";
+}
+
+const updateCodeExecutionStatistics = async (
+  requestData: CodeExecutionRequestData
+) => {
+  // we don't want to update execution statistics when running test code (like Jest)
+  if (process.env.NODE_ENV === "test") return;
+
+  let language: string = requestData.language;
+  if (language !== "cpp" && language !== "java" && language !== "py") {
+    // this shouldn't happen, but just in case someone sends
+    // a malicious language and we fail to catch it earlier
+    language = "unknown";
+  }
+
+  const updateDB = async () => {
+    await dynamoDBClient.send(
+      new UpdateItemCommand({
+        Key: {
+          id: {
+            S: "codeExecutions",
+          },
+        },
+        TableName: "online-judge-statistics",
+        // Note: make sure a map called `byDate` already exists inside the table...
+        UpdateExpression: `ADD totalCount :inc, #languageCount :inc, byDate.#dateCount :inc`,
+        ExpressionAttributeNames: {
+          "#languageCount": `${language}Count`,
+          "#dateCount": new Date().toISOString().split("T")[0], // Ex: 2021-10-31
+        },
+        ExpressionAttributeValues: {
+          ":inc": {
+            N: "1",
+          },
+        },
+      })
+    );
+  };
+
+  try {
+    await updateDB();
+  } catch (e) {
+    if (
+      e instanceof Error &&
+      e?.message ===
+        "The document path provided in the update expression is invalid for update"
+    ) {
+      // For us to do ADD byDate.#dateCount, the byDate map must exist
+      // If it doesn't exist and fails with the above error, create the byDate map and rerun the update
+      await dynamoDBClient.send(
+        new UpdateItemCommand({
+          Key: {
+            id: {
+              S: "codeExecutions",
+            },
+          },
+          TableName: "online-judge-statistics",
+          // Note: make sure a map called `byDate` already exists inside the table...
+          UpdateExpression: `SET byDate = :value`,
+          ConditionExpression: "attribute_not_exists(byDate)",
+          ExpressionAttributeValues: {
+            ":value": {
+              M: {},
+            },
+          },
+        })
+      );
+      await updateDB();
+    }
+  }
+};
 
 export const lambdaHandler = async (
   event: APIGatewayProxyEvent
@@ -36,6 +116,9 @@ export const lambdaHandler = async (
       }),
     };
   }
+
+  // deliberately async -- don't hold up the rest of the function execution while waiting for dynamodb
+  updateCodeExecutionStatistics(requestData);
 
   const compileCommand = new InvokeCommand({
     FunctionName: "online-judge-ExecuteFunction",
---
+++ submit/src/runLambdaLocally.ts
@@ -0,0 +1,22 @@
+// This file runs app.ts locally, and does not need AWS SAM to work
+// Run `npm run watch` followed by `node dist/src/runLambdaLocally.js`
+
+import * as app from "./app";
+import { generateRequest } from "./testUtils";
+
+process.env.NODE_ENV = "test";
+
+(async () => {
+  const result = await app.lambdaHandler(
+    generateRequest({
+      language: "cpp",
+      compilerOptions: "",
+      filename: "main.cpp",
+      sourceCode:
+        "#include <bits/stdc++.h>\nusing namespace std;\nint main(){int a, b, c; cin >> a >> b >> c; cout <<a+b+c << endl;}",
+      input: "1 2 3",
+    })
+  );
+
+  console.log(result);
+})();
